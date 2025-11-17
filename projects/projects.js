922c32a4f8425ed3a7681c0a1d7f0cdbc46a592c
chitic05 <chiticdavid05@gmail.com>
2025-11-17 22:34:37 +0200

    .

--- projects/projects.js
+++ projects/projects.js
@@ -4,8 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
             const cards = document.getElementsByClassName("project-card"); // nu ne da un array, ci un htmlcollection
             Array.from(cards).forEach(card => {  //convertim collectionul in array
             card.classList.remove("revealed");
-
             setTimeout(() => card.classList.add("revealed"), 10);
+            //am facut asta pentru ca broswerul pastreaza un cache si toate project card-urile
+            //vor avea clasa revealed mereu astfel nu se mai face animatia
             });
 
             const profileMenu = document.getElementById('profile');
