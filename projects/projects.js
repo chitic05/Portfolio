19bd08f2c407f98e91eb6ec04fb8ca6e0f409a21
chitic05 <chiticdavid05@gmail.com>
2025-11-17 12:47:36 +0200

    .

--- projects/projects.js
+++ projects/projects.js
@@ -1,8 +1,12 @@
 //Asigura ca s-a incarcat documentul
 document.addEventListener('DOMContentLoaded', () => {
             
+            const cards = document.getElementsByClassName("project-card"); // nu ne da un array, ci un htmlcollection
+            Array.from(cards).forEach(card => {  //convertim collectionul in array
+            card.classList.add("revealed");
+            });
+
             const profileMenu = document.getElementById('profile');
-            
             if (profileMenu) {
                 profileMenu.addEventListener('click', (event) => {
                     event.stopPropagation();
